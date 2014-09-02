4f822e817156cd881a81a818e62c0dbb878a4781
KatherineAnn <katemoschak@gmail.com>
2014-09-02 12:31:59 -0400

    Initial check-in

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	//create loop for appending children 
+
+	/*for (var i = 0; i < 9; i++) {
+
+		console.log(i);
+
+		setTimeout(function(){
+		var bigDiamond = document.createElement('div');
+
+		bigDiamond.className = 'bigDiamond';
+
+		document.getElementById('inner_main').appendChild(bigDiamond);
+
+	}, 100 * i);
+
+	}*/
+
+
+	//setting text to display to user
+	//document.getElementsByClassName("diamondText")[0].innerHTML = "Welcome";
+
+
+});
